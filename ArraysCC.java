9c7f8318468919d1e1b77cc490c80e52736925a6
manoj515876 <manojputtaluckey@gmail.com>
2024-01-17 14:41:58 +0530

    Array Imp answers

--- ArraysCC.java
+++ ArraysCC.java
@@ -2,45 +2,34 @@ import java.util.*;
 
 public class ArraysCC{
 
-    public static void maxSubArraySum(int numbers[]){
-        int currSum = 0;
-        int maxSum = Integer.MIN_VALUE;
-        int prefix[] = new int[numbers.length];
-        prefix[0] = numbers[0];
-        for(int i=1; i<numbers.length; i++){
-            prefix[i] = prefix[i-1] + numbers[i];
+    public static int trappedRainwater(int heights[]){
+        int n = heights.length;
+        int leftMax[] = new int[n];
+        leftMax[0] = heights[0];
+        for(int i=1; i<n; i++){
+            leftMax[i] = Math.max(heights[i], leftMax[i-1]);
         }
 
-        for(int i=0; i<numbers.length; i++){
-            for(int j=i; j<numbers.length; j++){
-                currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i-1];
-                if(currSum > maxSum){
-                    maxSum = currSum;
-                }
-            }
+        int rightMax[] = new int[n];
+        rightMax[n-1] = heights[n-1];
+        for(int i=n-2; i>=0; i--){
+            rightMax[i] = Math.max(heights[i], rightMax[i+1]);
         }
-        System.out.println("Max sum is : " + maxSum);
+
+        int trappedWater = 0;
+        for(int i=0; i<n; i++){
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += waterLevel - heights[i];
+        }
+
+        return trappedWater;
     }
 
-    public static void kadans(int numbers[]){
-        int cs = 0;
-        int ms = Integer.MIN_VALUE;
-        for(int i=0; i<numbers.length; i++){
-            cs += numbers[i];
-            ms = Math.max(cs, ms);
-            if(cs < 0){
-                cs = 0;
-            }
-            ms = Math.max(cs, ms);
-        }
-        System.out.println("Max sum is : " + ms);
-    }
+
 
     public static void main(String args[]){
-        // int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3};
-        
-        int numbers[] = {-2,1,-3,4,-1,2,1,-5,4};
-        kadans(numbers);
+        int heights[] = {4, 2, 0, 6, 3, 2, 5};
+        System.out.println(trappedRainwater(heights));
 
     }
 }
